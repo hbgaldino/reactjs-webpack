309b3ad4b66cb16500bc094b664472f7762115ed
Higor Galdino <hbgaldino@gmail.com>
2018-09-07 10:23:15 -0300

    Add webpack dev server and setup script to development mode

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<div>oi</div>, document.getElementById("root"));
+ReactDOM.render(<div>Xuxu</div>, document.getElementById("root"));
