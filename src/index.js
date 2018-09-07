c1e38bfd49fa3351a0970b4747414936d8386951
Higor Galdino <hbgaldino@gmail.com>
2018-09-07 17:10:05 -0300

    Add App component to encapsulate aplication

--- src/index.js
+++ src/index.js
@@ -5,19 +5,26 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Home from './js/pages/Home.jsx';
 import Dashboard from './js/pages/Dashboard.jsx';
 
-const app =
-    <Router>
-        <div>
-            <div>
-                <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/dashboard">Dashboard</Link></li>
-                </ul>
-            </div>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/dashboard" component={Dashboard} />
-        </div>
-    </Router>;
+class App extends React.Component {
 
+    render() {
+        return (
+            <Router>
+                <div>
+                    <h1>Título</h1>
+                    <div>
+                        <h2>Subtítulo</h2>
+                        <ul>
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to="/dashboard">Dashboard</Link></li>
+                        </ul>
+                    </div>
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/dashboard" component={Dashboard} />
+                </div>
+            </Router>
+        );
+    }
+}
 
-ReactDOM.render(app, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
