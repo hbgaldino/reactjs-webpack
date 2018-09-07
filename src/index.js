68007cb24a149272f3c89bcd267e3ce83ecd0fd3
Higor Galdino <hbgaldino@gmail.com>
2018-09-07 11:40:41 -0300

    Add react router and setup simple route in index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 import Home from './js/pages/Home.jsx';
+import Dashboard from './js/pages/Dashboard.jsx';
 
-ReactDOM.render(<Home /> , document.getElementById("root"));
+const app =
+    <Router>
+        <div>
+            <div>
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/dashboard">Dashboard</Link></li>
+                </ul>
+            </div>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/dashboard" component={Dashboard} />
+        </div>
+    </Router>;
+
+
+ReactDOM.render(app, document.getElementById("root"));
